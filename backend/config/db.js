4a677c2b41e734dba1ab86fea22fd552e6b6ccbd
AKN <arpankumarnandi@gmail.com>
2021-04-25 12:15:44 +0530

    Removing colors

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,18 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: true,
-    })
-    console.log(`MongoDB Connected: ${conn.connection.host}`.brightCyan.italic)
-  } catch (error) {
-    console.error(
-      `Error occured while connecting to MongoDB: ${error.message}`.red
-        .underline.bold
-    )
-    process.exit(1)
-  }
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: true
+		})
+		console.log(`MongoDB Connected: ${conn.connection.host}`)
+	} catch (error) {
+		console.error(`Error occured while connecting to MongoDB: ${error.message}`)
+		process.exit(1)
+	}
 }
 
 export default connectDB
